ab5640e7facfd69aa1396bd9ec5d832693597ac7
Jacob McAllister <datadeltaanalysis@protonmail.com>
2024-11-11 15:13:13 -0600

    Update
    Signed-off-by: Jacob McAllister <datadeltaanalysis@protonmail.com>

--- Week11_Polymorphism/src/PolymorphismIntro.java
+++ Week11_Polymorphism/src/PolymorphismIntro.java
@@ -9,10 +9,10 @@ public class PolymorphismIntro {
 		
 		tester.drive();
 
-//		tester.engineCheck();
+//		tester.engineCheck(); // Will throw a compilation error.
 
 		System.out.println("====================== CHECK INSTANCE TYPE ========================");
-//		 Using instanceof to check type at runtime
+		 
         if (tester instanceof Airplane) {
             System.out.println("The object is an instance of Airplane.");
         }
@@ -29,16 +29,18 @@ public class PolymorphismIntro {
         
 		// Can cast to allow us to us the runtime type of Airplane.
 		((Airplane) tester).engineCheck();
+		
 //
 //		// Can cast and place into "container" for more specific Vehicle
-//		Airplane testerAsPlane = (Airplane) tester;
+		Airplane testerAsPlane = (Airplane) tester;
 //
-//		testerAsPlane.deployLandingGear();
+		testerAsPlane.deployLandingGear();
 //
 //		// Notice no compilation errors.
-//		((Boat)tester).anchor();
+//		((Boat)tester).anchor(); // Will throw a runtime error.
 //
 		tester = new Boat(32, true, "Yacht");
+		tester.drive();
 		((Boat) tester).anchor();
 
 		System.out.println("====================== POLYMORPHISM ========================");
