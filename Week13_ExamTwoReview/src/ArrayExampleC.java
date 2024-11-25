800f417c690872b5245f8068bb48f439291b0974
Jacob McAllister <datadeltaanalysis@protonmail.com>
2024-11-25 10:27:29 -0600

    Class prep.
    Signed-off-by: Jacob McAllister <datadeltaanalysis@protonmail.com>

--- Week13_ExamTwoReview/src/ArrayExampleC.java
+++ Week13_ExamTwoReview/src/ArrayExampleC.java
@@ -2,7 +2,7 @@
 public class ArrayExampleC {
 
 	public static void main(String[] args) {
-		String[] values = { "apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi",
+		String[] values = { "apple", "null", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi",
 				"lemon", "mango", "nectarine" };
 
 		for (int i = 3; i <= values.length; i += 3) {
