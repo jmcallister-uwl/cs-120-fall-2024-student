ec2fac2be69580e728a07c17d9b94086d77a7eeb
Jacob McAllister <datadeltaanalysis@protonmail.com>
2024-11-25 22:50:06 -0600

    Class prep.
    Signed-off-by: Jacob McAllister <datadeltaanalysis@protonmail.com>

--- Week13_ExamTwoReview/src/ArrayExampleC.java
+++ Week13_ExamTwoReview/src/ArrayExampleC.java
@@ -2,12 +2,22 @@
 public class ArrayExampleC {
 
 	public static void main(String[] args) {
-		String[] values = { "apple", "null", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi",
+		
+		String[] values = { "apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi",
 				"lemon", "mango", "nectarine" };
 
 		for (int i = 3; i <= values.length; i += 3) {
 			System.out.println(values[i]);
 		}
+		
+		// i: 12
+		/*
+		 * cherry
+		 * grape
+		 * lemon
+		 * ERROR
+		 * 
+		 * */
 
 	}
 
--- Week13_ExamTwoReview/src/ArrayExampleD.java
+++ Week13_ExamTwoReview/src/ArrayExampleD.java
@@ -5,7 +5,8 @@ public class ArrayExampleD {
 		String[] words = new String[144];
 
 		for (int i = words.length - 1; i >= 0; i -= 3) {
-			System.out.println("words[" + i + "] length: " + words[i].length());
+			System.out.println("words[" + i + "] length: " 
+					+ words[i].length());
 		}
 
 	}
